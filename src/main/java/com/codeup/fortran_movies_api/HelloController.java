d795cd5dc32986ee0e46ee42102a55afdc541d44
Pat Barfield <patrickhbarfield@gmail.com>
2022-05-06 16:17:56 -0500

    spring intro

---
+++ src/main/java/com/codeup/fortran_movies_api/HelloController.java
@@ -0,0 +1,16 @@
+package com.codeup.fortran_movies_api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "Hello, from le Spring!";
+    }
+
+}
